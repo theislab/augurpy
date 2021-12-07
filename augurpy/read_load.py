fe9189259474ce965e9d2860dc38c7172b5272b8
antschum <32771191+antschum@users.noreply.github.com>
2021-12-07 14:35:09 +0100

    Data loading (#23)
    * load data
    
    * update dependencies
    
    * pass anndata obejct; add feature selection
    
    * run nox
    
    * remove Optional typing
    
    * Update augurpy/read_load.py
    
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>
    
    * Update augurpy/read_load.py
    
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>
    
    * remove  print status lines
    
    * remove as None from exception
    
    * print error and raise exception
    
    * run black - oempty last line was missing
    
    Co-authored-by: zethson <lukas.heumos@posteo.net>

--- augurpy/read_load.py
+++ augurpy/read_load.py
@@ -2,9 +2,15 @@ from typing import Union
 
 from anndata import AnnData
 from pandas import DataFrame
+from scanpy.preprocessing import highly_variable_genes
 
 
-def load(input: Union[AnnData, DataFrame], meta: DataFrame, label_col: str, cell_type_col: str) -> DataFrame:
+def load(
+    input: Union[AnnData, DataFrame],
+    meta: DataFrame,
+    label_col: str = "label_col",
+    cell_type_col: str = "cell_type_col",
+) -> AnnData:
     """Loads the input data.
 
     Args:
@@ -16,21 +22,52 @@ def load(input: Union[AnnData, DataFrame], meta: DataFrame, label_col: str, cell
         cell_type_col: column of the meta DataFrame or the Anndata or matrix containing the cell type labels for each
             cell in the cell-by-gene expression matrix
 
-
     Returns:
-        Pandas DataFrame containing gene expression values (cells in rows, genes in columns), cell type and condition
+        Anndata object containing gene expression values (cells in rows, genes in columns) and cell type, label as obs
     """
-    pass
+    if isinstance(input, AnnData):
+        input.obs = input.obs.rename(columns={cell_type_col: "cell_type", label_col: "label"})
+        out = input
+
+    elif isinstance(input, DataFrame):
+        try:
+            cell_type = input[cell_type_col]
+            label = input[label_col]
+        except KeyError:
+            print("[bold yellow]No column names matching cell_type_col and label_col. Looking in meta data.")
+            try:
+                cell_type = meta[cell_type_col]
+                label = meta[label_col]
+            except (KeyError, TypeError):
+                print("Missing label and / or cell type column.")
+                raise
+
+            else:
+                out = AnnData(X=input, obs={"cell_type": cell_type, "label": label})
+
+        else:
+            out = AnnData(X=input, obs={"cell_type": cell_type, "label": label})
+
+    else:
+        raise Exception("Not valid input.")
+
+    out = feature_selection(out)
+
+    return out
 
 
-def feature_selection(input: DataFrame, var_quantile: float) -> DataFrame:
+def feature_selection(input: AnnData) -> AnnData:
     """Feature selection by variance.
 
     Args:
         input: Pandas DataFrame containing gene expression values (cells in rows, genes in columns)
-         var_quantile: quantile of highly variable genes to retain for each cell type using the variable gene filter
 
     Results:
-        Pandas DataFrame with selected features
+        Anndata object with highly variable genes added as layer
     """
-    pass
+    min_features_for_selection = 1000
+
+    if len(input.var_names) - 2 > min_features_for_selection:
+        highly_variable_genes(input)
+
+    return input
