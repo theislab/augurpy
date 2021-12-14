1623194287fbafd270f43658f2639af6ff1ffb7d
Antonia Schumacher <32771191+antschum@users.noreply.github.com>
2021-12-14 13:38:13 +0100

    nox

--- tests/test_cv.py
+++ tests/test_cv.py
@@ -1,5 +1,7 @@
-import scanpy as sc
 from math import isclose
+
+import scanpy as sc
+
 from augurpy.cv import run_cross_validation
 from augurpy.estimators import Params, create_estimator
 from augurpy.read_load import load
@@ -16,5 +18,5 @@ rf_estimator = create_estimator("random_forest_classifier", Params(random_state=
 def test_run_cross_validation():
     """Test run cross validation with categorical and classifier."""
     cv = run_cross_validation(adata, rf_estimator, subsample_idx=1, folds=3)
-    auc = [0.8472222222222222, 0.7205882352941176, 0.6777777777777777]    
-    assert any([isclose(x, y, abs_tol=10**-15)for x, y in list(zip(cv["test_auc"].tolist(), auc))])
+    auc = [0.8472222222222222, 0.7205882352941176, 0.6777777777777777]
+    assert any([isclose(x, y, abs_tol=10 ** -15) for x, y in list(zip(cv["test_auc"].tolist(), auc))])
