f7fd60b57221ee420fb4ac443a3eab4431945d64
Antonia Schumacher <32771191+antschum@users.noreply.github.com>
2021-12-14 12:06:27 +0100

    add random state to params, test for specific score, add scanpy to poetry

--- tests/test_cv.py
+++ tests/test_cv.py
@@ -1,19 +1,19 @@
 import scanpy as sc
 
 from augurpy.cv import run_cross_validation
-from augurpy.estimators import create_estimator
+from augurpy.estimators import Params, create_estimator
 from augurpy.read_load import load
 
 adata = sc.read_h5ad("tests/sc_sim.h5ad")
 adata = load(adata)
 
 # subsample 100 obs
-sc.pp.subsample(adata, n_obs=100)
+sc.pp.subsample(adata, n_obs=100, random_state=42)
 
-rf_estimator = create_estimator("random_forest_classifier")
+rf_estimator = create_estimator("random_forest_classifier", Params(random_state=42))
 
 
 def test_run_cross_validation():
     """Test run cross validation with categorical and classifier."""
     cv = run_cross_validation(adata, rf_estimator, subsample_idx=1, folds=3)
-    assert cv["test_auc"] is not None
+    assert cv["test_auc"].tolist() == [0.8472222222222222, 0.7205882352941176, 0.6777777777777777]
